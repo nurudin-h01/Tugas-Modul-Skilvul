64e0944de88dc40adca286ef75ae6b10cb449144
nurudin-h01 <72251615+nurudin-h01@users.noreply.github.com>
2021-10-12 10:17:35 +0700

    Update 03-recursive.js

--- Tugas Modul 7 Javascript Intermediate/03-recursive/03-recursive.js
+++ Tugas Modul 7 Javascript Intermediate/03-recursive/03-recursive.js
@@ -19,22 +19,3 @@ searchInArray(arr, 1); // "angka ditemukan pada index: 0"
 searchInArray(arr, 8); // "angka tidak ditemukan"
 
 
-
-///4
-function trianglePattern(a, b) {
-    if (a === 0) {
-        return;
-    }
-    let space = "";
-    for (let i = 0; i < b; i++) {
-        if (i < a - 1) {
-            space += " "
-        } else {
-            space += String.fromCharCode(64 + (b - a + (i - a) + 2))
-        }
-    }
-    console.log(space);
-    trianglePattern(a - 1, b)
-}
-
-trianglePattern(5, 5);
