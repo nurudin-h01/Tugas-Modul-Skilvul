a31b0f29d067428a494d9d0b00143c0f8013279f
nurudin-h01 <72251615+nurudin-h01@users.noreply.github.com>
2021-10-25 06:47:13 +0700

    Tugas Modul 12 React Basic

---
+++ Tugas Modul 12 React Basic/soal-01/src/MovieCard.js
@@ -0,0 +1,42 @@
+import { Component } from "react"
+
+
+export default class MovieCard extends Component{
+    movie = {};
+    constructor(props){
+        super(props);
+        this.movie = props.movie
+    }
+    render(){
+        return(
+            <>
+            
+                <div class="card h-100">
+                    <img
+                        src={this.props.movie.image_url}
+                        class="card-img-top"
+                        alt="skilvul" />
+                    <div class="card-body">
+                        <h5 class="card-title">{this.props.movie.title}</h5>
+                        <h6 class="card-subtitle mb-2">
+                        <span className={this.props.movie.type=== "Movie" ? `badge bg-danger` :  `badge bg-success`}>{this.props.movie.type}</span>
+                        </h6>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make
+                            up the bulk of the card's content.
+                        </p>
+                    </div>
+                    <div class="card-body">
+                    <a
+                    href={this.props.movie.url}
+                    className="btn btn-primary w-100 text-uppercase"
+                >
+                            readmore
+                        </a>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+}
